f9c6cf2d3cf385b7dcda50e405052cc446a89940
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-14 22:59:07 -0700

    adds ducks every 3 seconds

--- app.js
+++ app.js
@@ -17,12 +17,27 @@ let autoUp={
     }
 }
 
+//Clicking the Duck
 function collectDucks(){
     ++ducks
-    ducks += clickUp.bread.multiplier * clickUp.bread.quantity
+    multipleDucks()
     update()
 }
 
+//Adds ducks
+function multipleDucks(){
+    ducks += clickUp.bread.multiplier*clickUp.bread.quantity
+}
+
+function collectAutoUp(){
+    console.log("got item")
+    update()
+    if(autoUp.pond.quantity >= 1){
+        ducks += autoUp.pond.multiplier*autoUp.pond.quantity
+    }
+}
+
+//Affects quantity and multiplier
 function buyBread(){
     if (ducks >= clickUp.bread.price){
         ducks -= clickUp.bread.price
@@ -36,24 +51,23 @@ function buyPond(){
     if (ducks >= autoUp.pond.price){
         ducks -= autoUp.pond.price
         ++autoUp.pond.quantity
-        ++autoUp.pond.multiplier
         autoUp.pond.price += 100
     }
     update()    
 }
 
 function startInterval(){
-    collectionInterval = setInterval(collectAutoUp, 3000)
-    
+    setInterval(collectAutoUp, 3000)
 }
+startInterval()
 
 function update(){
     document.getElementById("rubber").innerText = `${ducks}`
 
     document.getElementById("crumbs").innerText = `${clickUp.bread.quantity}`
     document.getElementById("breadBtn").innerText = `${clickUp.bread.price}`
-    document.getElementById("breadX").innerText = `${clickUp.bread.quantity}`
+    document.getElementById("breadX").innerText = `${clickUp.bread.multiplier}`
     document.getElementById("liquid").innerText = `${autoUp.pond.quantity}`
     document.getElementById("pondBtn").innerText = `${autoUp.pond.price}`
-    document.getElementById("pondX").innerText = `${autoUp.pond.quantity}`
+    document.getElementById("pondX").innerText = `${autoUp.pond.multiplier}`
 }
